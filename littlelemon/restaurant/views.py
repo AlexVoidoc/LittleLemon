1e87c8afb9227e872230a2635db24697dd5e9f31
AlexVoidoc <67524985+AlexVoidoc@users.noreply.github.com>
2023-08-31 18:05:26 -0400

    Implemented Djoser Authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets, permissions
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from . import models, serializers
 
 # Create your views here.
@@ -10,16 +11,14 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = models.Menu.objects.all()
     serializer_class = serializers.MenuSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Menu.objects.all()
     serializer_class = serializers.MenuSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = models.Booking.objects.all()
     serializer_class = serializers.BookingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
