a1d98998b67ca5d703d3173d0e52122c70627a0b
AlexVoidoc <67524985+AlexVoidoc@users.noreply.github.com>
2023-08-31 17:07:15 -0400

    Implemented rest framework and created endpoin;

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from . import models, serializers
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = models.Booking.objects.all()
+    serializer_class = serializers.BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
